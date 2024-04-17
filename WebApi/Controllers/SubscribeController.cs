d1e364f76ee80ccdd76d0e37a2c8b888fdb2d600
My <my.j97@protonmail.com>
2024-04-17 17:11:22 +0200

    Subscribe part

--- WebApi/Controllers/SubscribeController.cs
+++ WebApi/Controllers/SubscribeController.cs
@@ -1,21 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Infastructure.Contexts;
+using Infastructure.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApi.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class SubscribeController : ControllerBase
+public class SubscribeController(ApiContext context) : ControllerBase
 {
+    private readonly ApiContext _context = context;
+
     [HttpPost]
     public async Task<IActionResult> Subscribe(string email)
     {
-        return Ok();
+        if (ModelState.IsValid)
+        {
+            if (await _context.Subscribers.AnyAsync(x => x.Email == email))
+                return Conflict();
+
+            _context.Add(new SubscribersEntity { Email = email });
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+
+        return BadRequest();
     }
 
 
     [HttpDelete]
     public async Task<IActionResult> Unsubscribe(string email)
     {
-        return Ok();
+        if (ModelState.IsValid)
+        {
+
+        }
+
+
+        return BadRequest();
     }
 }
