2ad62fb1607f5e9ed91e0517d250f6b43f845d78
My <my.j97@protonmail.com>
2024-04-17 16:07:40 +0200

    Api Context done

---
+++ Infastructure/Contexts/ApiContext.cs
@@ -0,0 +1,10 @@
+﻿using Infastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace Infastructure.Contexts;
+
+public class ApiContext(DbContextOptions<ApiContext> options) : DbContext(options)
+{
+    public DbSet<SubscribersEntity> Subscribers { get; set; }
+}
---
+++ Infastructure/Entities/SubscribersEntity.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Infastructure.Entities;
+
+public class SubscribersEntity
+{
+    [Key]
+    public string Email { get; set; } = null!;
+}
