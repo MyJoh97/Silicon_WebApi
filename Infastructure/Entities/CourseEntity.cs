fe9aa0b358a0d138c2be0bffbb5657c9ee3c1b01
My <my.j97@protonmail.com>
2024-04-17 17:40:05 +0200

    CourseEntity

---
+++ Infastructure/Entities/CourseEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace Infastructure.Entities;
+
+public class CourseEntity
+{
+    public bool IsBestSeller { get; set; }
+    public string Image { get; set; } = null!;
+    public string Title { get; set; } = null!;
+    public string Author { get; set; } = null!;
+    public string Price { get; set; } = null!;
+    public string? DiscountPrice { get; set; }
+    public string Hours { get; set; } = null!;
+    public string LikesInProcent { get; set; } = null!;
+    public string LikesInNumbers { get; set; } = null!;
+}
