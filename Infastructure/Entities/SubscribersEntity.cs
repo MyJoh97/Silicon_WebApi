c0a48a638782c77d5acf857f39ab7b8fc934e4d3
My <my.j97@protonmail.com>
2024-04-19 18:40:47 +0200

    subcribers table added

--- Infastructure/Entities/SubscribersEntity.cs
+++ Infastructure/Entities/SubscribersEntity.cs
@@ -5,5 +5,12 @@ namespace Infastructure.Entities;
 public class SubscribersEntity
 {
     [Key]
+
     public string Email { get; set; } = null!;
+    public bool DailyNewsLetter { get; set; }
+    public bool AdvertisingUpdates { get; set; }
+    public bool WeekInReview { get; set; }
+    public bool EventUpdates { get; set; }
+    public bool StartupsWeekly { get; set; }
+    public bool Podcasts { get; set; }
 }
--- Infastructure/Migrations/20240419164019_Subscribers Table Updated.Designer.cs
+++ Infastructure/Migrations/20240419164019_Subscribers Table Updated.Designer.cs
@@ -3,6 +3,7 @@ using Infastructure.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infastructure.Migrations
 {
     [DbContext(typeof(ApiContext))]
-    partial class ApiContextModelSnapshot : ModelSnapshot
+    [Migration("20240419164019_Subscribers Table Updated")]
+    partial class SubscribersTableUpdated
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
